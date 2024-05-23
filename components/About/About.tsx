c5d3e5bdb061f65e6e4c733f963a6bf75063f430
Myles <myleshansen@outlook.com>
2024-05-22 20:40:04 -0700

    Fix sizing for smaller screens + mobile

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -2,36 +2,34 @@ import { useState } from "react";
 
 import Image from "next/image";
 import styles from "./About.module.css";
-import { FaAngleDoubleDown, FaArrowDown } from "react-icons/fa";
-import Link from "next/link";
 
 const About = () => {
   const [isHovering, setIsHovering] = useState(false);
   return (
-    <div id="about" className='flex max-h-[75vh]'>
+    <div id="about" className='flex max-h-[90vh]'>
       <div className='flex flex-col max-w-[50vw] mt-48 ml-20'>
         <div className='mb-20'>
-          <h1 className="font-raleway_e_light font-bold text-8xl">A little about <span style={{ color: "#3f9af7" }}>me</span>...</h1>
+          <h1 className="font-raleway_e_light font-bold text-[4.5vw] sm:text-[3.75vw]">A little about <span style={{ color: "#3f9af7" }}>me</span>...</h1>
         </div>
-        <div className='font-raleway_e_light text-3xl mb-12'>
+        <div className='font-raleway_e_light text-[2vw] sm:text-[1.2vw] mb-12'>
           <p>
             Current <span style={{ color: "#3f9af7" }}>software</span> engineer at Deloitte.
             Known mainly for security, blockchain/DeFi, AI, and full-stack engineering.
           </p>
         </div>
         <div>
-          <h3 className='font-raleway_e_light_italic text-4xl font-bold mb-12'>
+          <h3 className='font-raleway_e_light_italic text-[2.25vw] sm:text-[1.75vw] font-bold mb-12'>
             Some technologies I work with
           </h3>
           <div className='flex flex-row flex-wrap mt-4'>
-            <ul className='font-raleway_e_light text-2xl mr-16 mb-4 list-disc pl-[40px]'>
+            <ul className='font-raleway_e_light text-2xl mr-16 mb-4 list-disc pl-[40px] text-[2vw] sm:text-[1.2vw]'>
               <li>JavaScript (ES6+)/TypeScript</li>
               <li>Go</li>
               <li>React/Next.js</li>
               <li>Solidtiy</li>
               <li>Ethereum</li>
             </ul>
-            <ul className='font-raleway_e_light text-2xl mr-16 mb-4 list-disc pl-[40px]'>
+            <ul className='font-raleway_e_light text-2xl mr-16 mb-4 list-disc pl-[40px] text-[2vw] sm:text-[1.2vw]'>
               <li>Rust</li>
               <li>Python</li>
               <li>Java</li>
@@ -39,19 +37,19 @@ const About = () => {
               <li>PostgreSQL</li>
             </ul>
           </div>
-          <p className='font-raleway_e_light text-2xl mt-4'>
+          <p className='font-raleway_e_light text-[2vw] sm:text-[1.2vw] mt-4 max-w-[45vw]'>
             Outside of my work, I&apos;m interested in all facets of design. I
             also love staying active & exploring my many hobbies. I dabble in just about
             everything.
           </p>
           <div id="projects" className='flex items-center'>
-            <h1 className='font-raleway_bold text-8xl font-bold mt-8'>work<span style={{ color: "#3f9af7" }}>.</span></h1>
+            <h1 className='font-raleway_bold text-[4.5vw] sm:text-[3.75vw] font-bold mt-8'>work<span style={{ color: "#3f9af7" }}>.</span></h1>
           </div>
         </div>
       </div>
-      <div className={styles["about-right"]}>
+      <div className='flex items-center'>
         <div
-          className={styles["about-img-container"]}
+          className='hidden lg:block w-[25vw] h-[25vw] sm:w-[30vw] sm:h-[33vw] relative'
           onMouseEnter={() => setIsHovering(true)}
           onMouseLeave={() => setIsHovering(false)}
         >
@@ -60,7 +58,7 @@ const About = () => {
               src="/walking-towards.GIF"
               alt=""
               fill
-              className={styles["about-img"]}
+              className='border rounded-3xl w-[100%] h-[100%] relative object-cover'
               priority
             />
           ) : (
@@ -68,12 +66,12 @@ const About = () => {
               src="/walking-away.GIF"
               alt=""
               fill
-              className={styles["about-img"]}
+              className='border rounded-3xl w-[100%] h-[100%] relative object-cover'
               priority
             />
           )}
         </div>
-        <div className={styles.vl}></div>
+        <div className='hidden lg:block border border-black border-solid h-[75%] mt-4 ml-24'></div>
       </div>
     </div>
   );
--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <header>
       <nav className="flex flex-column items-center justify-between p-10 sm:px-16">
         <div className="">
-          <h1 className="font-jacquard text-[2vw]">H</h1>
+          <h1 className="font-jacquard text-5xl">H</h1>
         </div>
         <div className="hidden sm:flex sm:gap-5 sm:font-mono sm:font-thin sm:uppercase sm:text-[0.75rem]">
           <Link href="/blog">blog</Link>
